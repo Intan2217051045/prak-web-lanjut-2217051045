21d59f292d3acb9f16e525f6b61270714dd76724
Dewi Intan Nabila <112591952+Intan2217051045@users.noreply.github.com>
2024-09-12 12:49:33 +0700

    pesan commit

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+
+class ProfileController extends Controller
+{
+    public function profile($nama =' '){
+        $data =[
+        'nama'=> $nama,
+        'kelas'=>'A',
+        'npm'=>'2217051045'
+        ];
+
+        return view('profile', $data);
+
+    }
+}
